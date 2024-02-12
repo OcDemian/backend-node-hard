ad2564b2ef8eb92e62a36814b1854f8fb2b5f28e
Aleksandr <Pixar.1994@gmail.com>
2024-02-13 01:42:40 +0400

    add auth module content

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import {SequelizeModule} from "@nestjs/sequelize";
+import {User} from "../users/users.model";
+import {Role} from "../roles/roles.model";
+import {UserRoles} from "../roles/user-roles.model";
+import {RolesModule} from "../roles/roles.module";
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
+  imports: [
+    SequelizeModule.forFeature([User, Role, UserRoles]),
+    RolesModule
+  ]
 })
 export class AuthModule {}
