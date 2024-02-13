79f9c2e4d4c2cd58f5449dd9dcde9e4e3b663641
Aleksandr <Pixar.1994@gmail.com>
2024-02-13 19:02:00 +0400

    add auth guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,18 +1,29 @@
-import { Module } from '@nestjs/common';
+import {forwardRef, Module} from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import {SequelizeModule} from "@nestjs/sequelize";
 import {User} from "../users/users.model";
 import {Role} from "../roles/roles.model";
 import {UserRoles} from "../roles/user-roles.model";
-import {RolesModule} from "../roles/roles.module";
+import {UsersModule} from "../users/users.module";
+import {JwtModule} from "@nestjs/jwt";
+import * as process from "process";
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
-    SequelizeModule.forFeature([User, Role, UserRoles]),
-    RolesModule
+    forwardRef(() => UsersModule),
+    JwtModule.register({
+      secret:process.env.PRIVATE_KEY || 'SECRET',
+      signOptions: {
+        expiresIn: '24h'
+      }
+    })
+  ],
+  exports:[
+      AuthService,
+      JwtModule
   ]
 })
 export class AuthModule {}
--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ async function start(){
         .build()
     const document = SwaggerModule.createDocument(app, config)
     SwaggerModule.setup('/api/docs', app, document)
+
     await app.listen(PORT, () => console.log('Server started on port = ' + PORT))
 }
 
